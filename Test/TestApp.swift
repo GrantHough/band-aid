8b24ad3ebb0a74c4df656f7e21689523d903bdf9
Grant <grant.l.hough@gmail.com>
2021-07-29 13:28:20 -0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Grant Hough on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
